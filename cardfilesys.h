14126f84b381b1e40a27dc4de876d015777c6ea1
OscarSoDell7710\Oscar So <oscarso@hotmail.com>
2016-12-29 18:11:18 -0800

    Updated Object ID for device read/write data location

--- cardfilesys.h
+++ cardfilesys.h
@@ -4,8 +4,7 @@
 
 // Move into ykpiv.h later
 #define	szCARD_APPS						"cardapps"
-#define	YKPIV_OBJ_MSMD					0x5fd000
-#define YKPIV_OBJ_MSMDMSROOTS			(YKPIV_OBJ_MSMD + 1)
-#define	YKPIV_OBJ_MSMDCARDCF			(YKPIV_OBJ_MSMD + 2) // Variable Size:  6 bytes - 8KB or more
-#define	YKPIV_OBJ_MSMDCMAPFILE			(YKPIV_OBJ_MSMD + 3) // Variable Size:  6 bytes - 8KB or more
-#define	YKPIV_OBJ_RSAPUBKEYBLOB_OFFSET	(YKPIV_OBJ_MSMD + 4) // Variable Size:
+#define	YKPIV_OBJ_MS_CARDCF				0x5fff10
+#define	YKPIV_OBJ_MS_CMAPFILE			(YKPIV_OBJ_MS_CARDCF + 1)
+#define	YKPIV_OBJ_MS_MSROOTS			(YKPIV_OBJ_MS_CARDCF + 2)
+
--- yk_reset_tool.c
+++ yk_reset_tool.c
@@ -10,42 +10,56 @@ int	getDataOffset(const BYTE bContainerIndex) {
 	/*
 	One key container can hold only 1 key pairs (AT_SIGNATURE or AT_KEYEXCHANGE)
 	*/
-	return YKPIV_OBJ_RSAPUBKEYBLOB_OFFSET + bContainerIndex;
+	return (YKPIV_OBJ_MS_MSROOTS + 1) + bContainerIndex;
 }
 
 
 void clear_all_pages(ykpiv_state*	state) {
-	ykpiv_rc		ykrc = YKPIV_OK;
-	int				objID;
-	unsigned char	buf[1024 * 2];
+	ykpiv_rc	ykrc = YKPIV_OK;
+	int			objID;
+	char		buf[1];
 
 	memset(buf, 0, sizeof(buf));
 
-	printf("Clearing cardcf ...");
-	objID = YKPIV_OBJ_MSMDCARDCF;
+	objID = YKPIV_OBJ_MS_CARDCF;
+	printf("Clearing cardcf (0x%x) ...", objID);
 	ykrc = ykpiv_save_object(state, objID, buf, sizeof(buf));
 	if (ykrc == YKPIV_OK) { printf("Done.\n"); }
 	else { printf("Failed.\n"); }
 
-	printf("Clearing cmapfile ...");
-	objID = YKPIV_OBJ_MSMDCMAPFILE;
+	objID = YKPIV_OBJ_MS_CMAPFILE;
+	printf("Clearing cmapfile (0x%x) ...", objID);
 	ykrc = ykpiv_save_object(state, objID, buf, sizeof(buf));
 	if (ykrc == YKPIV_OK) { printf("Done.\n"); }
 	else { printf("Failed.\n"); }
 
-	printf("Clearing msroots ...");
-	objID = YKPIV_OBJ_MSMDMSROOTS;
+	objID = YKPIV_OBJ_MS_MSROOTS;
+	printf("Clearing msroots (0x%x) ...", objID);
 	ykrc = ykpiv_save_object(state, objID, buf, sizeof(buf));
 	if (ykrc == YKPIV_OK) { printf("Done.\n"); }
 	else { printf("Failed.\n"); }
 
+	objID = YKPIV_OBJ_AUTHENTICATION;
+	printf("Clearing YKPIV_OBJ_AUTHENTICATION (0x%x) ...", objID);
+	ykrc = ykpiv_save_object(state, objID, buf, sizeof(buf));
+	if (ykrc == YKPIV_OK) { printf("Done.\n"); }
+	else { printf("Failed.\n"); }
+
+	objID = YKPIV_OBJ_SIGNATURE;
+	printf("Clearing YKPIV_OBJ_SIGNATURE (0x%x) ...", objID);
+	ykrc = ykpiv_save_object(state, objID, buf, sizeof(buf));
+	if (ykrc == YKPIV_OK) { printf("Done.\n"); }
+	else { printf("Failed.\n"); }
+
+#if 0
 	for (int cntrIndex = 0; cntrIndex < 5; cntrIndex++) {
 		objID = getDataOffset(cntrIndex);
-		printf("Clearing at ContainerIndex %d in %x ...", cntrIndex, objID);
+		printf("Clearing at ContainerIndex %d in 0x%x ...", cntrIndex, objID);
 		ykrc = ykpiv_save_object(state, objID, buf, sizeof(buf));
 		if (ykrc == YKPIV_OK) { printf("Done.\n"); }
 		else { printf("Failed.\n"); }
 	}
+#endif
 
 	printf("\n\n\nPress Any Key to Continue\n");
 	getchar();
@@ -60,7 +74,9 @@ int main(void)
 	ykpiv_state*	ykState;
 	int				verbosity = 0;
 	int				retries = 0;
-	char			key[] = { 0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08 };
+	char			key[] = { 0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,
+		                      0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,
+		                      0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08 };
 	char			pin[9] = { "aaaaaaaa" };
 
 
@@ -89,6 +105,21 @@ int main(void)
 
 	//clear all pages
 	clear_all_pages(ykState);
+
+	//sign at 9c
+	/*
+	unsigned char	pt[256];
+	size_t			ptLen = sizeof(pt);
+	unsigned char	sig[256];
+	size_t			sigLen = sizeof(sig);
+	ykrc = ykpiv_sign_data(
+			ykState,
+			pt, ptLen,
+			&sig[0], &sigLen,
+			YKPIV_ALGO_RSA2048,
+			0x9c
+			);
+	*/
 
 	printf("All Done\n");
 	ykpiv_done(ykState);
